3699942b9c1986fba65124ebf4aa1e90c8ad2e78
RAGHAV ANTHAL <92143499+RAGHAV-223@users.noreply.github.com>
2025-02-24 15:29:52 +0530

    Update backend.js

--- backend/backend.js
+++ backend/backend.js
@@ -16,9 +16,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: "https://student-sphere-frontend.vercel.app",// Frontend's URL
-    credentials: true, 
+    origin: ["https://student-sphere-frontend.vercel.app"],// Frontend's URL
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true, 
     allowedHeaders: ["Content-Type", "Authorization"],
 };
 
