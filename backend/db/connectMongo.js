aa89146f70bdcd7c8fd409b4200de14e6615ad8a
PREET PAUL SHARMA <95271767+NOBODY1819@users.noreply.github.com>
2025-02-26 22:46:32 +0530

    Update connectMongo.js
    Making connection presistent

--- backend/db/connectMongo.js
+++ backend/db/connectMongo.js
@@ -1,12 +1,20 @@
 import mongoose from "mongoose";
 
 const connectMongo = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_DB_URL);
-        console.log("Connected to MongoDB")
-    } catch (error) {
-        console.log("Error: ", error.message);
-    }
+   if (mongoose.connection.readyState >= 1) {
+    console.log("✅ Already connected to MongoDB");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Failed:", error);
+    process.exit(1);
+  }
 }
 
 export default connectMongo
