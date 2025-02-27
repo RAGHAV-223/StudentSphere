6e3a007ed720a65d6cf66a2dcd4a27843711fbf4
PREET PAUL SHARMA <95271767+NOBODY1819@users.noreply.github.com>
2025-02-27 18:41:18 +0530

    error fixing

--- backend/db/connectMongo.js
+++ backend/db/connectMongo.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
 const connectMongo = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_DB_URL);
-        console.log("Connected to MongoDB")
-    } catch (error) {
-        console.log("Error: ", error.message);
-    }
+   if (mongoose.connection.readyState === 1) {
+    console.log("🔄 MongoDB already connected!");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URL);
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Failed:", error);
+    process.exit(1); // Exit if unable to connect
+  }
 }
 
 export default connectMongo
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'; // Import global styles if any
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-window.API_BASE_URL = "https://student-sphere-omega.vercel.app";
+window.API_BASE_URL = import.meta.env.API_URL;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
