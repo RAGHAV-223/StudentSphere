4d839026dfeb12ba5b4683cdf21aedbf8d09248a
RAGHAV-223 <raghavanthal8836@gmail.com>
2025-03-06 19:29:12 +0530

    Removed bootstrap

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; // Import global styles if any
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 window.API_BASE_URL = import.meta.env.API_URL;
 
