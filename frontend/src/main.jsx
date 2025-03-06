ae17c9b1d76bbde4763c8d1207c59b92f62b5d88
RAGHAV-223 <raghavanthal8836@gmail.com>
2025-03-06 18:30:58 +0530

    update main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,8 @@ import App from './App';
 import './index.css'; // Import global styles if any
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import dotenv from 'dotenv';
 
-dotenv.config()
-window.API_BASE_URL = process.env.API_URL;
+window.API_BASE_URL = import.meta.env.API_URL;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
